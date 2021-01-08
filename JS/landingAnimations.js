d92b278e6fbc0dc59ceab931dd2feab5eeb90507
Nuno Sousa <9180579@esmad.ipp.pt>
2021-01-08 02:58:50 +0000

    Landing animations being developed

--- JS/landingAnimations.js
+++ JS/landingAnimations.js
@@ -1,7 +1,28 @@
+// gsap.registerPlugin(ScrollTrigger);
+
 let tl = gsap.timeline({
   defaults: {
     ease: Power1.easeInOut
   }
+});
+
+ScrollTrigger.defaults({
+  toggleActions: "restart pause resume reset",
+
+  markers: true
+});
+
+gsap.to(".what", {
+  scrollTrigger: {
+    trigger: ".what",
+    // toggleActions: "resume pause resume resume",
+    start: "-150% 80%",
+    // end: "center center",
+    markers: true
+    // scrub: true
+  },
+  y: "10%",
+  duration: 1
 });
 
 window.addEventListener("load", () => {
@@ -29,30 +50,94 @@ window.addEventListener("load", () => {
       [".path"],
       {
         strokeDashoffset: 0,
-        duration: 1.25,
+        duration: 2,
         opacity: 1
       },
-      "-=1.5"
+      "-=3"
     )
-    .to([".text_category"], {
-      y: "15%",
-      duration: 0.6
-    })
-    .to([".day"], {
-      y: "0%",
-      duration: 1
-    })
-    .to([".info_date"], {
-      opacity: 1,
-      x: "0%",
-      duration: 1
-    });
-  // .to(
-  //   [".info_date"],
-  //   {
-  //     y: "0%",
-  //     duration: 0.6
-  //   },
-  //   "-=4.65"
-  // );
+    .to(
+      [".text_category"],
+      {
+        y: "15%",
+        duration: 0.6
+      },
+      "-=0.75"
+    )
+    .to(
+      [".day"],
+      {
+        y: "15%",
+        duration: 1
+      },
+      "-=0.75"
+    )
+    .to(
+      [".info_date"],
+      {
+        opacity: 1,
+        x: "0%",
+        duration: 1
+      },
+      "-=0.75"
+    )
+    .to(
+      [".descLine"],
+      {
+        opacity: 1,
+        y: "0%",
+        duration: 1
+      },
+      "-=0.75"
+    )
+    .to(
+      [".info_date"],
+      {
+        y: "0%",
+        duration: 0.6
+      },
+      "-=0.75"
+    );
 });
+//   [".path"],
+//   {
+//     scrollTrigger: ".path",
+//     strokeDashoffset: 0,
+//     duration: 2,
+//     opacity: 1
+//   },
+//   "-=2.5"
+// )
+//   .to(
+//     [".text_category"],
+//     {
+//       y: "15%",
+//       duration: 0.6
+//     },
+//     "-=1"
+//   )
+//   .to(
+//     [".day"],
+//     {
+//       y: "0%",
+//       duration: 1
+//     },
+//     "-=1"
+//   )
+//   .to(
+//     [".info_date"],
+//     {
+//       opacity: 1,
+//       x: "0%",
+//       duration: 1
+//     },
+//     "-=1"
+//   )
+//   .to(
+//     [".descLine"],
+//     {
+//       opacity: 1,
+//       y: "0%",
+//       duration: 1
+//     },
+//     "-=0.5"
+//   );
--- JS/onOff.js
+++ JS/onOff.js
@@ -74,7 +74,7 @@ function soundControl() {
     }, 250);
   } else {
     setTimeout(() => {
-      audio.volume = 0.25;
+      audio.volume = 0;
     }, 250);
   }
 }
